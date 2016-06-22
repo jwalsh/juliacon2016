17b5b267e693c4a491c7fe84726d75aa1edac04e
Jason Walsh <j@wal.sh>
2016-06-22 19:51:53 +0100

    Tests

---
+++ forwarddiff.jl_fast_derivatives_made_easy_jarrett_revels/tests.js
@@ -0,0 +1,24 @@
+var tests = {
+  cube:  function(x) {
+    return x * x * x;
+  },
+
+  identity: function(x) {
+    return x;
+  },
+  sin: function(x) {
+    return Math.sin(x);
+  },
+  cos: function(x) {
+    return Math.cos(x);
+  },
+  pow: function(x) {
+    return Math.exp(10, x);
+  },
+  e: function(x) {
+
+  },
+  ln: function(x) {
+
+  }
+};
